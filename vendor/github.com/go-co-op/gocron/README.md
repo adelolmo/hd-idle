6c17bf8a38aaa4d8d863ba976dd5fd65ea1a85b8
Andoni del Olmo <andoni.delolmo@gmail.com>
2020-06-07 08:14:18 +0200

    move to go-co-op gocron

---
+++ vendor/github.com/go-co-op/gocron/README.md
@@ -0,0 +1,108 @@
+# goCron: A Golang Job Scheduling Package.
+
+[![CI State](https://github.com/go-co-op/gocron/workflows/Go%20Test/badge.svg)](https://github.com/go-co-op/gocron/actions?query=workflow%3A"Go+Test") ![Go Report Card](https://goreportcard.com/badge/github.com/go-co-op/gocron)
+
+goCron is a Golang job scheduling package which lets you run Go functions periodically at pre-determined interval using a simple, human-friendly syntax.
+
+goCron is a Golang implementation of Ruby module [clockwork](https://github.com/tomykaira/clockwork) and Python job scheduling package [schedule](https://github.com/dbader/schedule).
+
+See also this two great articles:
+
+- [Rethinking Cron](http://adam.herokuapp.com/past/2010/4/13/rethinking_cron/)
+- [Replace Cron with Clockwork](http://adam.herokuapp.com/past/2010/6/30/replace_cron_with_clockwork/)
+
+If you want to chat, you can find us at Slack! [<img src="https://img.shields.io/badge/gophers-gocron-brightgreen?logo=slack">](https://gophers.slack.com/archives/CQ7T0T1FW)
+
+Examples:
+
+```go
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-co-op/gocron"
+)
+
+func task() {
+    fmt.Println("I am running task.")
+}
+
+func taskWithParams(a int, b string) {
+    fmt.Println(a, b)
+}
+
+func main() {
+    s1 := gocron.NewScheduler(time.UTC)
+    s1.Every(3).Seconds().Do(task)
+    <- s1.Start() // starts running (blocks current thread)
+
+    // Do jobs without params
+    s2 := gocron.NewScheduler(time.UTC)
+    s2.Every(1).Second().Do(task)
+    s2.Every(2).Seconds().Do(task)
+    s2.Every(1).Minute().Do(task)
+    s2.Every(2).Minutes().Do(task)
+    s2.Every(1).Hour().Do(task)
+    s2.Every(2).Hours().Do(task)
+    s2.Every(1).Day().Do(task)
+    s2.Every(2).Days().Do(task)
+    s2.Every(1).Week().Do(task)
+    s2.Every(2).Weeks().Do(task)
+
+    // Do jobs with params
+    s2.Every(1).Second().Do(taskWithParams, 1, "hello")
+
+    // Do jobs on specific weekday
+    s2.Every(1).Monday().Do(task)
+    s2.Every(1).Thursday().Do(task)
+
+    // Do a job at a specific time - 'hour:min:sec' - seconds optional
+    s2.Every(1).Day().At("10:30").Do(task)
+    s2.Every(1).Monday().At("18:30").Do(task)
+    s2.Every(1).Tuesday().At("18:30:59").Do(task)
+
+    // Begin job at a specific date/time. 
+    // Attention: scheduler timezone has precedence over job's timezone!
+    t := time.Date(2019, time.November, 10, 15, 0, 0, 0, time.UTC)
+    s2.Every(1).Hour().StartAt(t).Do(task)
+
+    // use .StartImmediately() to run job upon scheduler start
+    s2.Every(1).Hour().StartImmediately().Do(task)
+
+
+    // NextRun gets the next running time
+    _, time := s2.NextRun()
+    fmt.Println(time)
+
+    // Remove a specific job
+    s2.Remove(task)
+
+    // Clear all scheduled jobs
+    s2.Clear()
+
+    // Start all the pending jobs
+    <- s2.Start()
+}
+```
+
+and full test cases and [document](http://godoc.org/github.com/jasonlvhit/gocron) will be coming soon (help is wanted! If you want to contribute, pull requests are welcome).
+
+If you need to prevent a job from running at the same time from multiple cron instances (like running a cron app from multiple servers),
+you can provide a [Locker implementation](example/lock.go) and lock the required jobs.
+
+```go
+gocron.SetLocker(lockerImplementation)
+gocron.Every(1).Hour().Lock().Do(task)
+```
+
+Looking to contribute? Try to follow these guidelines:
+ * Use issues for everything
+ * For a small change, just send a PR!
+ * For bigger changes, please open an issue for discussion before sending a PR.
+ * PRs should have: tests, documentation and examples (if it makes sense)
+ * You can also contribute by:
+    * Reporting issues
+    * Suggesting new features or enhancements
+    * Improving/fixing documentation
---
+++ vendor/github.com/go-co-op/gocron/job.go
@@ -0,0 +1,122 @@
+package gocron
+
+import (
+	"errors"
+	"time"
+)
+
+// Error declarations for gocron
+var (
+	ErrTimeFormat           = errors.New("time format error")
+	ErrParamsNotAdapted     = errors.New("the number of params is not adapted")
+	ErrNotAFunction         = errors.New("only functions can be schedule into the job queue")
+	ErrPeriodNotSpecified   = errors.New("unspecified job period")
+	ErrParameterCannotBeNil = errors.New("nil paramaters cannot be used with reflection")
+)
+
+// Job struct keeping information about job
+type Job struct {
+	interval          uint64                   // pause interval * unit between runs
+	startsImmediately bool                     // if the job should run upon scheduler start
+	jobFunc           string                   // the job jobFunc to run, func[jobFunc]
+	unit              timeUnit                 // time units, ,e.g. 'minutes', 'hours'...
+	atTime            time.Duration            // optional time at which this job runs
+	err               error                    // error related to job
+	lastRun           time.Time                // datetime of last run
+	nextRun           time.Time                // datetime of next run
+	startDay          time.Weekday             // Specific day of the week to start on
+	funcs             map[string]interface{}   // Map for the function task store
+	fparams           map[string][]interface{} // Map for function and  params of function
+	lock              bool                     // lock the job from running at same time form multiple instances
+	tags              []string                 // allow the user to tag jobs with certain labels
+	time              timeHelper               // an instance of timeHelper to interact with the time package
+}
+
+// NewJob creates a new job with the time interval.
+func NewJob(interval uint64) *Job {
+	th := newTimeHelper()
+	return &Job{
+		interval: interval,
+		lastRun:  th.Unix(0, 0),
+		nextRun:  th.Unix(0, 0),
+		startDay: time.Sunday,
+		funcs:    make(map[string]interface{}),
+		fparams:  make(map[string][]interface{}),
+		tags:     []string{},
+		time:     th,
+	}
+}
+
+// True if the job should be run now
+func (j *Job) shouldRun() bool {
+	return j.time.Now().Unix() >= j.nextRun.Unix()
+}
+
+//Run the job and immediately reschedule it
+func (j *Job) run() {
+	j.lastRun = j.time.Now()
+	go callJobFuncWithParams(j.funcs[j.jobFunc], j.fparams[j.jobFunc])
+}
+
+// Err should be checked to ensure an error didn't occur creating the job
+func (j *Job) Err() error {
+	return j.err
+}
+
+// Tag allows you to add labels to a job
+// they don't impact the functionality of the job.
+func (j *Job) Tag(t string, others ...string) {
+	j.tags = append(j.tags, t)
+	for _, tag := range others {
+		j.tags = append(j.tags, tag)
+	}
+}
+
+// Untag removes a tag from a job
+func (j *Job) Untag(t string) {
+	newTags := []string{}
+	for _, tag := range j.tags {
+		if t != tag {
+			newTags = append(newTags, tag)
+		}
+	}
+
+	j.tags = newTags
+}
+
+// Tags returns the tags attached to the job
+func (j *Job) Tags() []string {
+	return j.tags
+}
+
+func (j *Job) periodDuration() (time.Duration, error) {
+	interval := time.Duration(j.interval)
+	var periodDuration time.Duration
+
+	switch j.unit {
+	case seconds:
+		periodDuration = interval * time.Second
+	case minutes:
+		periodDuration = interval * time.Minute
+	case hours:
+		periodDuration = interval * time.Hour
+	case days:
+		periodDuration = interval * time.Hour * 24
+	case weeks:
+		periodDuration = interval * time.Hour * 24 * 7
+	default:
+		return 0, ErrPeriodNotSpecified
+	}
+	return periodDuration, nil
+}
+
+// NextScheduledTime returns the time of when this job is to run next
+func (j *Job) NextScheduledTime() time.Time {
+	return j.nextRun
+}
+
+// GetWeekday returns which day of the week the job will run on
+// This should only be used when .Weekday(...) was called on the job.
+func (j *Job) GetWeekday() time.Weekday {
+	return j.startDay
+}
